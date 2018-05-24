90559f0598fdc82f8a148ad57cd301f30deaa6bc
Ramon Moraes <ramon.moraes94@gmail.com>
2018-05-24 15:33:04 -0300

    animated

--- src/sort/QuickSort.js
+++ src/sort/QuickSort.js
@@ -7,50 +7,57 @@ export default class QuickSort {
     this.sort(this.array, 0, len - 1);
     console.log(this.array);
   }
+
   loop() {
 
   }
-  sort(array, left, right) {
-    var pivot = this.partitionLomuto(array, left, right); // you can play with both partition
 
-    if (left < pivot - 1) {
-      this.sort(array, left, pivot - 1);
-    }
-    if (right > pivot) {
-      this.sort(array, pivot, right);
-    }
-    return array;
+  sort(array, left, right) {
+    this.partitionLomuto(array, left, right)
+      .then(pivot => {
+        if (left < pivot - 1) {
+          this.sort(array, left, pivot - 1);
+        }
+        if (right > pivot) {
+          this.sort(array, pivot, right);
+        }
+        return array;
+      })
   }
 
   partitionLomuto(array, left, right) {
-    const pivot = right;
-    let i = left;
-    let j;
-    for (j = left; j < right; j++) {
-      if (array[j] <= array[pivot]) {
-        this.swap(i, j);
-        i++;
-      }
-    }
+    return new Promise( (resolve, reject) => {
+      setTimeout(() => {
 
-    this.swap(i, j);
-    return i;
+        const pivot = right;
+        let i = left;
+        let j;
+
+        for (j = left; j < right; j++) {
+          if (array[j] <= array[pivot]) {
+            this.swap(i, j);
+            i++;
+          }
+        }
+
+        this.swap(i, j)
+        resolve(i);
+      },450)
+    });
   }
 
   swap(i, j) {
     const temp = this.array[i];
     this.array[i] = this.array[j];
     this.array[j] = temp;
-    window.requestAnimationFrame(() => {
-      this.callAnimation
-    })
+    this.callAnimation();
   }
 
   callAnimation() {
     if (this.requestAnimationCallback) {
-      setTimeout( ()=> {
+      setTimeout(() => {
         this.requestAnimationCallback(this.array);
-      },15)
+      }, 15)
     }
   }
 }
