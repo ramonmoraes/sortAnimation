af31228aad3df271903e0aec2c51e305d927551c
Ramon Moraes <ramon.moraes94@gmail.com>
2018-06-05 22:00:51 -0300

    ....

--- src/sort/QuickSort.js
+++ src/sort/QuickSort.js
@@ -1,16 +1,15 @@
 "use strict";
-const arr = [11, 9, 5, 2, 7, 1, 4, 12, 17, 3, 8, 6];
+// const arr = [11, 9, 5, 2, 7, 4, 12, 17, 3, 8, 6, 0, 100, 1];
 // const arr = [1, 3, 5, 2, 7, 9, 6, 8, 4, 11, 23, 44,213,54,43,21,10];
 
 // const arr = [4, 3, 2, 1, 0];
-
+  const arr = [3,2,2,1];
 // export default
 class QuickSort {
   constructor() {
     this.array = [...arr];
     const len = this.array.length;
     const sorted = this.sort(this.array, 0, len - 1);
-    console.log(this.array);
   }
 
   sort(array, begin, end) {
@@ -18,7 +17,7 @@ class QuickSort {
     let i = begin - 1;
     let j = begin;
     let sorting = true;
-    if(array.length === 1 || begin >= end) {
+    if (array.length === 1 || begin >= end) {
       return array;
     }
 
@@ -32,23 +31,10 @@ class QuickSort {
         sorting = false;
       }
     }
-
     this.swap(i + 1, pivot);
-    // console.log({
-    //   begin,
-    //   end,
-    //   i,
-    //   j
-    // })
-    // setTimeout(()=> {
-      this.sort(this.array, begin, i);
-    // },1500)
-    // setTimeout(()=> {
-      this.sort(this.array, i + 1, end);
-    // },1500)
-    
+    this.sort(this.array, begin, i);
+    this.sort(this.array, i + 1, end);
     console.log(this.array);
-    
     // const smallerArray = this.array.slice(begin, i + 2);
     // const biggerArray = this.array.slice(i + 2, end);
     // console.log(smallerArray);
